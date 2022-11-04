8c5f53f6d9dfa70b527ecd69887e9390be320e8b
maheshtamarakar <maheshtamarakar369@gmail.com>
2022-11-04 10:38:17 +0530

    added seller auth signup page

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerAuthComponent implements OnInit {
 
+  // tips 
+  // always make sure to make the return type of function
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  signUp(data:object):void {
+    console.warn(data);
+    
+  }
+
+
 
 }
