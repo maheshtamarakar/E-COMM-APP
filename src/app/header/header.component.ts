8d81ceaed5a5fb7103410d8526106d1d3c36d447
maheshtamarakar <maheshtamarakar369@gmail.com>
2022-11-10 11:31:00 +0530

    added logout
    1. routing for add product
    2. routing for product list
    3, added product component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,22 +7,34 @@ import { Router } from '@angular/router'
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  menuType: String = 'default';
+  menuType: string = 'default';
+  sellerName: string = ''
   constructor(private route: Router) { }
 
   ngOnInit(): void {
+
     // Imp new learning*********** using router service for checking route change****************
-    this.route.events.subscribe((val: any)=>{
+    this.route.events.subscribe((val: any)=>{      
       if(val.url){
-        if(localStorage.getItem('seller') && val.url.includes('seller')){
+        let sellerStore = localStorage.getItem('seller')
+        if(sellerStore && val.url.includes('seller')){
           console.log("in seller area");
           this.menuType = 'seller'
+          let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+          this.sellerName = sellerData.name;
+          
         }else{
           console.log("out of seller area");
           this.menuType = 'default'
         }
       }
     })
+  }
+
+  // LogOut function
+  logout(){
+    localStorage.removeItem('seller')
+    this.route.navigate(['/'])
   }
 
 }
