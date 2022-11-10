71020b28f0a2b5d05c4f479d8ec11484dba64657
maheshtamarakar <maheshtamarakar369@gmail.com>
2022-11-10 10:58:49 +0530

    added seller condition to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  menuType: String = 'default';
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+    // Imp new learning*********** using router service for checking route change****************
+    this.route.events.subscribe((val: any)=>{
+      if(val.url){
+        if(localStorage.getItem('seller') && val.url.includes('seller')){
+          console.log("in seller area");
+          this.menuType = 'seller'
+        }else{
+          console.log("out of seller area");
+          this.menuType = 'default'
+        }
+      }
+    })
   }
 
 }
