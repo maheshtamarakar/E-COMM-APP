85ff4962162fe0d8765ebdb524084434369ba8fd
VishnuTamrakar <maheshtamarakar369@gmail.com>
2023-02-03 09:39:04 +0530

    added detail search component

--- src/app/header/search/search.component.ts
+++ src/app/header/search/search.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/data-type';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchResult: Product[] = [];
+  constructor(
+    private _activeRoute: ActivatedRoute,
+    private _productService: ProductService
+  ){}
+
+  ngOnInit(): void {
+    let query = this._activeRoute.snapshot.paramMap.get('query')
+    query && this._productService.searchProduct(query).subscribe((result)=>{
+      console.log('result', result);
+      
+      this.searchResult = result;
+    })
+  }
 
 }
